a8cdcc82e38acccc98f74d353ee3a315179cda9a
Ivan Porto Carrero <ivan@flanders.co.nz>
2018-07-08 18:56:11 -0700

    better output for commit log

--- cmd/tpt/cmd/bundle_log.go
+++ cmd/tpt/cmd/bundle_log.go
@@ -6,9 +6,11 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/fatih/color"
+	"github.com/gosuri/uitable"
 	"github.com/oneconcern/trumpet/pkg/store"
 	"github.com/spf13/cobra"
 )
@@ -56,22 +58,22 @@ func shortLogFormatter() FormatterFunc {
 
 func longLogFormatter() FormatterFunc {
 	return func(w io.Writer, data interface{}) error {
+		table := uitable.New()
+		table.MaxColWidth = 80
+		table.Wrap = true
+
 		for _, c := range data.([]store.Bundle) {
-			fmt.Fprint(w, "     ID: ")
-			fmt.Fprintln(w, color.MagentaString(c.ID))
-			fmt.Fprint(w, "Authors: ")
-			for i, v := range c.Committers {
-				if i > 0 {
-					fmt.Fprint(w, ", ")
-				}
-				fmt.Fprint(w, color.YellowString(v.String()))
+			table.AddRow("ID:", color.MagentaString(c.ID[:70])+"...")
+			var authors []string
+			for _, v := range c.Committers {
+				authors = append(authors, color.YellowString(v.String()))
 			}
-			fmt.Fprintln(w)
-			fmt.Fprint(w, "   Date: ")
-			fmt.Fprintln(w, color.YellowString(c.Timestamp.Format(time.RFC3339)))
-			fmt.Fprintln(w)
-			fmt.Fprintln(w, c.Message)
+			table.AddRow("Authors:", strings.Join(authors, ", "))
+			table.AddRow("Date:", color.YellowString(c.Timestamp.Format(time.RFC3339)))
+			table.AddRow("")
+			table.AddRow("", c.Message)
 		}
+		fmt.Fprintln(w, table.String())
 		return nil
 	}
 }
