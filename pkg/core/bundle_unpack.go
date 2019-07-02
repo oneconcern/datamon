a592596829a10e50ce43e76831743ac3e2c663c2
Ransom Williams <rwilliams@oneconcern.com>
2019-07-02 14:49:18 -0700

    wip -- parallelize bundle file list download

--- pkg/core/bundle_unpack.go
+++ pkg/core/bundle_unpack.go
@@ -36,7 +36,111 @@ func unpackBundleDescriptor(ctx context.Context, bundle *Bundle) error {
 	return nil
 }
 
+type downloadBundleFileListChans struct {
+	bundleEntries      chan<- model.BundleEntries
+	error              chan<- error
+	done               <-chan struct{}
+	doneOk             chan<- struct{}
+	concurrencyControl <-chan struct{}
+}
+
+func downloadBundleFileListFile(ctx context.Context, bundle *Bundle,
+	chans downloadBundleFileListChans,
+	i uint64,
+) {
+	var bundleEntries model.BundleEntries
+
+	sendErr := func(err error) {
+		select {
+		case chans.error <- err:
+		case <-chans.done:
+		}
+	}
+	defer func() {
+		<-chans.concurrencyControl
+	}()
+
+	bundle.l.Info("downloading bundle entry",
+		zap.Uint64("curr entry", i),
+		zap.Uint64("tot entries", bundle.BundleDescriptor.BundleEntriesFileCount),
+	)
+	bundleEntriesBuffer, err := storage.ReadTee(ctx,
+		bundle.MetaStore, model.GetArchivePathToBundleFileList(bundle.RepoID, bundle.BundleID, i),
+		bundle.ConsumableStore, model.GetConsumablePathToBundleFileList(bundle.BundleID, i))
+	if err != nil {
+		sendErr(err)
+		return
+	}
+	err = yaml.Unmarshal(bundleEntriesBuffer, &bundleEntries)
+	if err != nil {
+		sendErr(err)
+		return
+	}
+	select {
+	case chans.bundleEntries <- bundleEntries:
+	case <-chans.done:
+	}
+
+}
+
+func downloadBundleFileList(ctx context.Context, bundle *Bundle,
+	chans downloadBundleFileListChans,
+) {
+	var i uint64
+	// todo: percolate constant to ui
+	concurrencyControl := make(chan struct{}, 8)
+	chans.concurrencyControl = concurrencyControl
+	for i = 0; i < bundle.BundleDescriptor.BundleEntriesFileCount; i++ {
+		concurrencyControl <- struct{}{}
+		go downloadBundleFileListFile(ctx, bundle, chans, i)
+	}
+	for i := 0; i < cap(concurrencyControl); i++ {
+		concurrencyControl <- struct{}{}
+	}
+	chans.doneOk <- struct{}{}
+}
+
+// ??? orig impl guarantees returning the entries in a particular order.
+//    .. and this could as well, provided the total number of bundle entires doesn't exceed uint64 max.
+// for now, foregoing ordering in order not to have limit on bundle entries based on num bits.
 func unpackBundleFileList(ctx context.Context, bundle *Bundle) error {
+
+	// todo: pass  param as in implUpload()
+	var bundleEntriesPerFile uint64 = defaultBundleEntriesPerFile
+
+	bundleEntriesC := make(chan model.BundleEntries)
+	errorC := make(chan error)
+	doneC := make(chan struct{})
+	doneOkC := make(chan struct{})
+
+	defer close(doneC)
+
+	go downloadBundleFileList(ctx, bundle, downloadBundleFileListChans{
+		bundleEntries: bundleEntriesC,
+		error:         errorC,
+		done:          doneC,
+		doneOk:        doneOkC,
+	})
+
+	// prealloc
+	bundle.BundleEntries = make([]model.BundleEntry, 0,
+		bundle.BundleDescriptor.BundleEntriesFileCount*bundleEntriesPerFile)
+	var gotDoneSignal bool
+	for !gotDoneSignal {
+		select {
+		case bundleEntries := <-bundleEntriesC:
+			bundle.BundleEntries = append(bundle.BundleEntries, bundleEntries.BundleEntries...)
+		case err := <-errorC:
+			bundle.l.Error("Unpack bundle filelist failed", zap.Error(err))
+			return err
+		case <-doneOkC:
+			gotDoneSignal = true
+		}
+	}
+	return nil
+}
+
+func unpackBundleFileListOrig(ctx context.Context, bundle *Bundle) error { // nolint: deadcode, unused
 	// Download the files json
 	var i uint64
 	for i = 0; i < bundle.BundleDescriptor.BundleEntriesFileCount; i++ {
