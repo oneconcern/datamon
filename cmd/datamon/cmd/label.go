c8580420eb619be7e28512d419bf6be45fdc0afa
Ritesh H Shukla <ritesh@oneconcern.com>
2020-01-29 14:40:17 -0800

    Update label.go

--- cmd/datamon/cmd/label.go
+++ cmd/datamon/cmd/label.go
@@ -32,7 +32,7 @@ func init() {
 	rootCmd.AddCommand(labelCmd)
 
 	labelDescriptorTemplate = func() *template.Template {
-		const listLineTemplateString = `{{.Name}} , {{.BundleID}} , {{.Timestamp}} {{printf "\n"}}`
+		const listLineTemplateString = `{{.Name}} , {{.BundleID}} , {{.Timestamp}}`
 		return template.Must(template.New("list line").Parse(listLineTemplateString))
 	}()
 }
