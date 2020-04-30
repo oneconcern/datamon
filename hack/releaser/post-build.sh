fa7af0221e57a3141f4bdf9e85717c46cdff4937
Ritesh H Shukla <ritesh@oneconcern.com>
2020-04-30 11:20:01 -0700

    fix releaser bug
    Signed-off-by: Ritesh H Shukla <ritesh@oneconcern.com>

--- hack/releaser/post-build.sh
+++ hack/releaser/post-build.sh
@@ -8,4 +8,4 @@
 # goreleaser post hook does not run in the build context
 # to resolve templated attributes.
 # See github.com/goreleaser/goreleaser/issues/1261
-find dist -type f -name "$1" -0|xargs upx
+find dist -type f -name "$1" -exec xargs upx {} ';'
